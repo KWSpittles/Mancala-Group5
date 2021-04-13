fe2b84ce07ac96790ceb98db2c38492ee084ed44
julian8897-swansea <78489417+julian8897-swansea@users.noreply.github.com>
2021-04-13 16:19:57 +0100

    Initial commit test for user and admin classes

---
+++ src/sample/Admin.java
@@ -0,0 +1,37 @@
+package sample;
+
+import java.util.ArrayList;
+
+public class Admin extends User {
+	
+	private ArrayList<User> userList;
+	
+	public Admin(String userName, String first, String last) {
+		super(userName, first, last);
+	}
+	
+	//method to add user to an ArrayList
+	public void addUser(User user) {
+    	userList.add(user);
+    }
+	
+	//method to remove user from ArrayList
+	public void removeUser(User user) {
+		userList.remove(user);
+	}	
+	
+	//method to check if the username is valid
+	public Boolean CheckUsername(String UserName) {  
+		boolean check = false;
+	    if(UserName == getUserName()) {
+	    	check = true;
+	    }
+	    return check;
+	}
+	
+	public String toString() {
+		return ("Number of current users :" + getuserCount());
+	}
+    	
+}
+
--- src/sample/User.java
+++ src/sample/User.java
@@ -4,17 +4,112 @@ import javafx.scene.image.Image;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.UUID;
 
 public class User {
 
-    private int PlayerID;
-    private String Username;
+    private final UUID PlayerID;
+    private Date firstDate;
+    
+    private String UserName;
     private String firstName;
     private String lastName;
-    private Date firstDate;
     private Image profilePicture;
+    private String password;
     private int[] winLossDraw;
-    private ArrayList<User> favPlayers;
+    private static int userCount = 0; //counter to track the number of users
+    //private ArrayList<Player> favPlayers = new ArrayList();;
+    
 
+    public User (String userName, String first, String last){
+    	UserName = userName;
+    	firstName = first;
+    	lastName = last;
+    	PlayerID = UUID.randomUUID(); //generates a random unique identifier
+    	//password = pass;
+       	
+    	userCount++; //adds one to the user count each time the constructor is called
+    }
+    
+//    public void generateID() {
+//    	if (PlayerID == null) {
+//    		PlayerID = UUID.randomUUID();
+//    	}
+//      }
+    
+    //Setters
+    public void setUserName(String UserName) {
+    	this.UserName = UserName;
+    } 
+    
+    public void setfirstName(String firstName) {
+    	this.firstName = firstName;
+    }
+    
+    public void setlastName(String lastName) {
+    	this.lastName = lastName;
+    	
+    }
+    
+    //sets the password
+    public void setPassword(String password) {
+    	this.password = password;
+    	
+    }
+       
+    public void setprofilePicture(Image profilePicture) {
+    	this.profilePicture = profilePicture;
+    }
+    
+    public void setwinLossDraw(int[] winLossDraw) {
+    	this.winLossDraw = winLossDraw;
+    }
+    
+    //getters
+    //get user name
+    public String getUserName() {
+    	return UserName;
+    } 
+    
+    //get first name
+    public String getfirstName() {
+    	return firstName;
+    } 
+    
+    //get last name
+    public String getlastName() {
+    	return lastName;
+    } 
+    
+    //get password
+    public String getPassword() {
+    	return password;
+    } 
+    
+    //needs to retrieve this info from login system
+    public Date getfirstDate() {
+    	return firstDate;
+    }
+    
+    //Gets the generated unique ID 
+    public UUID getPlayerID() {
+    	return PlayerID;
+    }
+    
+    //gets the number of current users
+    public static int getuserCount() {
+    	return userCount;
+    	
+    }
+    
+    public String toString() {
+		return ("Username :" + UserName + "\nFirst name :" + firstName + "\nLast name :" + lastName + "\nWins/Losses/Draws :" 
+				+ winLossDraw);
+	}
+    
+//    public double winPercentage() {
+//    	double winPercent = 0;
+//    	return winPercent;
+//    }
+    
 }
-
