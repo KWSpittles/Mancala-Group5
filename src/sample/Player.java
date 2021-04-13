6d5fdf8fecccbb63da32b72243ee548f806973a1
Kieren <kwspittles@googlemail.com>
2021-04-13 13:30:02 +0100

    Test commit by updating user/player classes

--- src/sample/Player.java
+++ src/sample/Player.java
@@ -1,9 +1,22 @@
-//package sample;
-//
-//public class Player extends User {
-//
-//
-//
-//
-//
-//}
+package sample;
+
+import javafx.scene.image.Image;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Player extends User {
+
+    private int PlayerID;
+    private String Username;
+    private String firstName;
+    private String lastName;
+    private Date firstDate;
+    private Image profilePicture;
+    private int[] winLossDraw;
+    private ArrayList<User> favPlayers;
+
+
+
+
+
+}
--- src/sample/User.java
+++ src/sample/User.java
@@ -1,33 +1,20 @@
-//package sample;
-//
-//import javafx.scene.image.Image;
-//
-//import java.util.ArrayList;
-//import java.util.Date;
-//
-//public class User {
-//
-//    private int PlayerID;
-//    private String Username;
-//    private String firstName;
-//    private String lastName;
-//    private Date firstDate;
-//    private Image profilePicture;
-//    private int[] winLossDraw;
-//    private ArrayList<Player> favPlayers;
-//
-//    public User (String username){
-//
-//    }
-//
-//
-//
-//
-//
-//
-//
-//
-//}
-//
-//
-//    Date firstDate = new Date();
+package sample;
+
+import javafx.scene.image.Image;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class User {
+
+    private int PlayerID;
+    private String Username;
+    private String firstName;
+    private String lastName;
+    private Date firstDate;
+    private Image profilePicture;
+    private int[] winLossDraw;
+    private ArrayList<User> favPlayers;
+
+}
+
